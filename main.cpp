1538b76b116a5cec969c9218f35e411599ae9e9c
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-12-01 11:47:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+    int numero;
+    cin>>numero;
+    if (funzione(numero,2)==1)
+        cout<<"numero primo";
+    else cout<<"numero non primo";
   return 0;
 }
